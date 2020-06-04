0601dac41eb673ee55191cb75adefeb6025c085f
alexanderig <alexander.ignatyuk@gmail.com>
2020-06-04 18:42:09 +0300

    Add project files.

---
+++ Semaphores/SemaphoresForm.Designer.cs
@@ -0,0 +1,164 @@
+﻿//using System.Windows.Forms;
+
+namespace Semaphores
+{
+    partial class SemaphoresForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+                
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(22, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(255, 354);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 25;
+            this.listBox2.Location = new System.Drawing.Point(305, 59);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(255, 354);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 25;
+            this.listBox3.Location = new System.Drawing.Point(587, 59);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(255, 354);
+            this.listBox3.TabIndex = 2;
+            this.listBox3.DoubleClick += new System.EventHandler(this.listBox3_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Работающие задачи";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(329, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ожидающие задачи";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(608, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(201, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Созданные задачи";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 445);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(315, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Количество мест в семафоре:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(589, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 46);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Создать поток";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(381, 445);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // SemaphoresForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 514);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "SemaphoresForm";
+            this.Text = "Семафоры и задачи";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Semaphores/SemaphoresForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Semaphores
+{
+
+    public partial class SemaphoresForm : Form
+    {
+
+        class MyTasksParam
+        {
+            public MyTasksParam(string mess)
+            {
+                isCancelTask = false;
+                TskMessage = mess;
+            }
+            public bool isCancelTask { get; set; }
+            public string TskMessage { get; set; }
+        }
+
+        List<MyTasksParam> workingtasks;
+        List<Task> disposetasks;
+        const int maxsize = 100;
+        Semaphore semaphore;
+        public SynchronizationContext curcontext;
+        int Index { get; set; }
+        int curSize { get; set; }
+        List<Task> tasklist;
+        public SemaphoresForm()
+        {
+            InitializeComponent();
+            semaphore = new Semaphore(0, 100, "1A9191BF-AA26-46E1-BB85-BDA396BC6469");
+            Index = 0;
+            curSize = 0;
+            curcontext = SynchronizationContext.Current;
+            tasklist = new List<Task>();
+            workingtasks = new List<MyTasksParam>();
+            disposetasks = new List<Task>();
+        }
+
+        private void SomeJobBusy(Object obj)
+        {
+            Semaphore sem = Semaphore.OpenExisting("1A9191BF-AA26-46E1-BB85-BDA396BC6469");
+            MyTasksParam tobj = (MyTasksParam)obj;
+            sem.WaitOne();
+
+        }
+
+
+        private void SomeJob(Object obj)
+        {
+            try
+            {
+                Semaphore sem = Semaphore.OpenExisting("1A9191BF-AA26-46E1-BB85-BDA396BC6469");
+                MyTasksParam tobj = (MyTasksParam)obj;
+                string str = tobj.TskMessage;
+                curcontext.Send(t => listBox2.Items.Add(str), null);
+                sem.WaitOne();
+                curcontext.Send(t => listBox2.Items.Remove(str), null);
+                int counter = 0, idx = 0;
+                str = str.Substring(0, (str.IndexOf('>') + 2)) + counter;
+                curcontext.Send(t => listBox1.Items.Add(str), null);
+                workingtasks.Add(tobj);
+                
+                while (counter < 101)
+                {
+
+                    curcontext.Send(t => {
+                        idx = listBox1.Items.IndexOf(str);
+                         if (idx > -1)
+                         {
+                            str = str.Substring(0, (str.IndexOf('>') + 2)) + counter + '%';
+                            listBox1.Items.RemoveAt(idx);
+                            listBox1.Items.Insert(idx, str);
+                         }
+
+                    }, null);
+                   
+                    Thread.Sleep(150);
+                    if (tobj.isCancelTask)
+                        break;
+                    counter++;
+                }
+                curcontext.Send(t => listBox1.Items.Remove(str), null);
+                if(!tobj.isCancelTask)
+                sem.Release();
+                workingtasks.Remove(tobj);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Index < maxsize)
+            {
+                string mess = "Задача " + (Index + 1) + " --> создана";
+                string mess2 = "Задача " + (Index + 1) + " --> ожидает";
+                MyTasksParam obj = new MyTasksParam(mess2);
+                tasklist.Add(new Task(SomeJob, obj));
+                listBox3.Items.Add(mess);
+                Index++;
+                
+            }
+            else
+                MessageBox.Show("Achieved MaxSize of 100 tasks");
+           
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int nvalue = (int)numericUpDown1.Value;
+            if (nvalue - curSize == 1)
+                  semaphore.Release(1);
+            else if(workingtasks.Count > nvalue)
+                workingtasks[0].isCancelTask = true;
+            else
+            {
+                MyTasksParam obj = new MyTasksParam("IdleTask");
+                disposetasks.Add(Task.Factory.StartNew(SomeJobBusy, obj));
+                
+            }
+            curSize = nvalue;            
+        }
+
+        private void listBox3_DoubleClick(object sender, EventArgs e)
+        {
+            if (tasklist.Count > 0 && listBox3.SelectedIndex >= 0)
+            {
+                int idx = listBox3.SelectedIndex;
+                tasklist.ElementAt(idx).Start();
+                disposetasks.Add(tasklist.ElementAt(idx));
+                tasklist.RemoveAt(idx);
+                listBox3.Items.RemoveAt(idx);
+            }
+        }
+
+        ~SemaphoresForm()
+        {
+            foreach (var tsk in disposetasks)
+            {
+                tsk.Dispose();                
+            }
+        }
+    }
+}
